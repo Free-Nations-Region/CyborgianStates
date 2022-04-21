cce49e123d7cb9205123b70fe7596e4079b3ff11
heaveria <me@heaveria.com>
2022-04-20 22:01:09 -0400

    #60 Boilerplate for the ce command.

---
+++ CyborgianStates.Tests/CommandTests/CouldEndorseCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace CyborgianStates.Tests.CommandTests;
+
+public class CouldEndorseCommand : BaseCommandTests
+{
+    
+
+    public CouldEndorseCommand()
+    {
+        
+
+    }
+    
+    [Fact]
+    public override async Task TestExecuteSuccess()
+    {
+        
+        
+    }
+
+    [Fact]
+    public override async Task TestCancel()
+    {
+        
+    }
+
+    [Fact]
+    public override async Task TestExecuteWithErrors()
+    {
+        
+    }
+}
