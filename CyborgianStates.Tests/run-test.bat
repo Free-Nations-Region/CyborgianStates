2161f89b821c2965c05cb204339d572a67d15578
drehtisch <38186302+drehtisch@users.noreply.github.com>
2023-07-19 18:47:24 +0200

    Cleanup

--- CyborgianStates.Tests/run-test.bat
+++ CyborgianStates.Tests/run-test.bat
@@ -10,7 +10,7 @@ REM Note that some coverage may not be detected correctly by reportgenerator
 REM
 dotnet clean ..\CyborgianStates\CyborgianStates.sln
 dotnet build ..\CyborgianStates\CyborgianStates.sln
-coverlet bin\Debug\net6.0\CyborgianStates.Tests.dll --target "dotnet" --targetargs "test --no-build" --exclude "[*]CyborgianStates.Wrapper.*" --exclude "[*]CyborgianStates.Data.*" --exclude "[*]CyborgianStates.AppSettings" --exclude "[NationStatesSharp]*" --format opencover --output "./TestResults//"
+coverlet bin\Debug\net7.0\CyborgianStates.Tests.dll --target "dotnet" --targetargs "test --no-build" --exclude "[*]CyborgianStates.Wrapper.*" --exclude "[*]CyborgianStates.Data.*" --exclude "[*]CyborgianStates.AppSettings" --exclude "[NationStatesSharp]*" --format opencover --output "./TestResults//"
 pause
 reportgenerator -reports:TestResults/coverage.opencover.xml -targetdir:TestResults/Reports -reporttypes:HtmlInline
 start TestResults/Reports/index.htm
--- CyborgianStates/Data/Sqlite/Sqlite_SeedDb.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO "StatusNames"("Name") VALUES ('added');
-INSERT INTO "StatusNames"("Name") VALUES ('pending');
-INSERT INTO "StatusNames"("Name") VALUES ('send');
-INSERT INTO "StatusNames"("Name") VALUES ('api-queued');
-INSERT INTO "StatusNames"("Name") VALUES ('reserved');
-INSERT INTO "StatusNames"("Name") VALUES ('skipped');
-INSERT INTO "StatusNames"("Name") VALUES ('verified');
